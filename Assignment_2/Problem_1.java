17edd278d4296a1ad5af3dd44b2761c311fc5c26
anushka99rai-svg <anushka99rai@gmail.com>
2026-08-09 18:01:59 +0530

    Update Problem_1.java

--- Assignment_2/Problem_1.java
+++ Assignment_2/Problem_1.java
@@ -1,1 +1,53 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class problem_1 {
+    static int n;
+    static int k;
+    static int[] keys;
+    static ArrayList<Integer>[] adj;
+    static int ans = 0;
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        if (!sc.hasNextInt()) return;
+        
+        n = sc.nextInt();
+        k = sc.nextInt();
+
+        keys = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            keys[i] = sc.nextInt();
+        }
+
+        adj = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adj[u].add(v);
+            adj[v].add(u);
+        }
+
+        dfs(1, 0, 0);
+
+        System.out.println(ans);
+    }
+
+    static void dfs(int node, int parent, int currentXor) {
+        int xorVal = currentXor ^ keys[node];
+
+        if (xorVal >= k) {
+            ans++;
+        }
+
+        for (int neighbor : adj[node]) {
+            if (neighbor != parent) {
+                dfs(neighbor, node, xorVal);
+            }
+        }
+    }
+}
